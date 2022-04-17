fe49376aaee2b64fcab20b89e14b18563c4c598e
loukgoldberg <69332571+loukgoldberg@users.noreply.github.com>
2022-04-17 13:58:23 -0700

    Update Pilot_Code.ino

--- Pilot_Code.ino
+++ Pilot_Code.ino
@@ -10,12 +10,10 @@ void setup() {
   bleKeyboard.begin();
 }
 
-// Pretty sure the below loop is awful and has to be improved a lot but it works! Probably not power efficient.
-
 void loop() {
   Wire.requestFrom(0x08, 1);
-  if (Wire.available()) { // can also use while but idk which is better
-    uint8_t key_val = Wire.read();                  // receive a byte as character
+  if (Wire.available()) {
+    uint8_t key_val = Wire.read(); // receive a byte as character
     if(key_val != 0) {
       bleKeyboard.print((char)key_val);
     }
